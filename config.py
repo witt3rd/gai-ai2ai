ca7a0d5ddb38e7a3acb5ceb51021ed4913a7c7c6
witt3rd <donald@witt3rd.com>
2023-08-06 07:42:45 -0700

    fix .env.template

--- config.py
+++ config.py
@@ -7,19 +7,21 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-@lru_cache()
-def get_config():
-    return Settings(
-        OPENAI_API_KEY=os.getenv("OPENAI_API_KEY"),
-        DATETIME_FORMAT=os.getenv("DATETIME_FORMAT", "%Y-%m-%dT%H:%M:%S.%fZ"),
-        SESSION_DIR=os.getenv("SESSION_DIR", "./sessions"),
-        TRANSCRIPT_DIR=os.getenv("TRANSCRIPT_DIR", "./transcripts"),
-    )
-
-
 @dataclass
 class Settings:
     OPENAI_API_KEY: str
     DATETIME_FORMAT: str
     SESSION_DIR: str
     TRANSCRIPT_DIR: str
+    PROMPTS_FILE: str
+
+
+@lru_cache()
+def get_config() -> Settings:
+    return Settings(
+        OPENAI_API_KEY=os.getenv("OPENAI_API_KEY"),
+        DATETIME_FORMAT=os.getenv("DATETIME_FORMAT", "%Y-%m-%dT%H:%M:%S.%fZ"),
+        SESSION_DIR=os.getenv("SESSION_DIR", "./sessions"),
+        TRANSCRIPT_DIR=os.getenv("TRANSCRIPT_DIR", "./transcripts"),
+        PROMPTS_FILE=os.getenv("PROMPTS_FILE", "prompts.json"),
+    )
